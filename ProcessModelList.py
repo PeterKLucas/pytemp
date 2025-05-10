04e2994611f1d05f050c050370dfc20f2dbeae19
PeterKLucas <61668422+PeterKLucas@users.noreply.github.com>
2025-05-10 17:00:44 -0400

    updated commit

--- ProcessModelList.py
+++ ProcessModelList.py
@@ -6,7 +6,7 @@ import pandas as pd                 # data processing
 import numpy as np                  # working with arrays
 import matplotlib.pyplot as plt     # visualization
 from matplotlib import rcParams     # figure size
-
+from termcolor import colored as cl # text formatting
 
 #-------------------------------------------------------------#
 #scikit-learn
